7e0b125ceb11be312a2107287a4774b4c2099a44
TsubasaOura <Tsubasa.Gacct@gmail.com>
2021-07-24 22:31:50 +0900

    [mod] devise_token_auth (#19)

---
+++ api/app/controllers/api/auth/registrations_controller.rb
@@ -0,0 +1,16 @@
+module Api
+    module Auth
+      class RegistrationsController < DeviseTokenAuth::RegistrationsController
+          
+        private
+        def sign_up_params
+          params.permit(:name, :email, :password, :password_confirmation, :role_id, :user_detail_id)
+        end
+  
+        def account_update_params
+          params.permit(:name, :email)
+        end
+  
+      end
+    end
+  end
