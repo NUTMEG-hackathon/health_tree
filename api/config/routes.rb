0233686c083cd1d25505a3f64f8cecbf887901a2
TsubasaOura <Tsubasa.Gacct@gmail.com>
2021-07-25 02:02:51 +0900

    [delete] (#34)

--- api/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  namespace :api do
-    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-      registrations: 'api/auth/registrations'
-    }
-  end
-end
